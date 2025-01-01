049019366ab2ffd09456c66abd40a16eeffe65f0
ddty <aboubakrdatty@gmail.com>
2025-01-01 14:04:00 +0100

    First commit

---
+++ src/app/generate/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button"
+
+
+function page() {
+  return (
+    <div className=' min-h-screen flex justify-center items-center'>
+        <Button className="bg-blue-500">Generate</Button>
+    </div>
+  )
+}
+
+export default page
