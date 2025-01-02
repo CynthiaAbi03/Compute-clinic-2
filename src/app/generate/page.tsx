a4192bcc783dfbd9965737a78201303e03b64976
ddty <aboubakrdatty@gmail.com>
2025-01-02 01:01:11 +0100

    Add background and differents librairies

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -1,12 +1,25 @@
-import { Button } from "@/components/ui/button"
-
+import Image from "next/image";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+//import { Button } from "@/components/ui/button"
+import bgcompute from "../../../public/image.png";
 
 function page() {
   return (
-    <div className=' min-h-screen flex justify-center items-center'>
-        <Button className="bg-blue-500">Generate</Button>
+    <div className="h-screen">
+      <div className="h-1/3 w-full flex">
+        <AspectRatio ratio={16 / 9}>
+          <Image
+            src={bgcompute}
+            alt="Image"
+            className="w-full h-1/3 object-cover"
+          />
+          
+        </AspectRatio>
+        
+      </div>
+      <div className="bg-green-200 h-2/3 w-full">2</div>
     </div>
-  )
+  );
 }
 
-export default page
+export default page;
